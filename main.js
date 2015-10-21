49d27f7225775efe90a79cc696f33550815aeda2
Rob Russo <rrusso44@gmail.com>
2015-10-21 01:43:41 -0400

    Removed Semicolon
    The Chrome Dev Editor marks this is unnecessary. Thanks for the example!  Saved me on a project

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function updateWebviews() {
   var webview = document.querySelector("webview");
   webview.style.height = document.documentElement.clientHeight + "px";
   webview.style.width = document.documentElement.clientWidth + "px";
-};
+}
 
 onload = updateWebviews;
 window.onresize = updateWebviews;
